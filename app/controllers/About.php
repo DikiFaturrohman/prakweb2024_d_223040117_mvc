f50e89572e0f2909fbd631c6c5b9968b0ef3abbb
Diki Faturrohman <118197085+DikiFaturrohman@users.noreply.github.com>
2024-10-26 08:27:23 +0700

    Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,10 @@
+<?php
+
+class About{
+    public function index($nama, $pekerjaan){
+        echo "Halo, Nama saya $nama, saya adalah seorang $pekerjaan";
+    }
+    public function page(){
+        echo 'About/page';
+    }
+}
