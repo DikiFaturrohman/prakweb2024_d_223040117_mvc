385f89a47b6536836d3f4d28bb17784c57cf5105
Diki Faturrohman <118197085+DikiFaturrohman@users.noreply.github.com>
2024-10-28 09:05:15 +0700

    Assets

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb2024_d_223040117_mvc/public');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
-
-    <h1>About Me.</h1>
+<div class="container">
+    
+    <h1 class="mt-4">About Me.</h1>
+    <img src="C:\laragon\www\prakweb2024_d_223040117_mvc\public\img\profile.jpg" alt="Diki" width="200" class="rounded-circle shadow">
     <p>Halo, nama saya <?= $data['nama'];?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
