ca9f6cc36ecbdbe5f30c800d0936566bf5842502
Diki Faturrohman <118197085+DikiFaturrohman@users.noreply.github.com>
2024-10-30 12:54:03 +0700

    Insert Data

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -23,5 +23,27 @@ class Mahasiswa_model {
             $this->db->bind('id', $id);
             return $this->db->single();
         }
+
+        public function tambahDataMahasiswa($data)
+        {
+            // Make sure all fields are present
+            if (empty($data['nama']) || empty($data['nrp']) || empty($data['email']) || empty($data['jurusan'])) {
+                return 0; // Optionally handle the error (e.g., throw an exception or log it)
+            }
+        
+            $query = "INSERT INTO mahasiswa (nama, nrp, email, jurusan)
+                      VALUES (:nama, :nrp, :email, :jurusan)";
+        
+            $this->db->query($query);
+            $this->db->bind('nama', $data['nama']);
+            $this->db->bind('nrp', $data['nrp']);
+            $this->db->bind('email', $data['email']);
+            $this->db->bind('jurusan', $data['jurusan']);
+        
+            $this->db->execute();
+        
+            return $this->db->rowCount();
+        }
+        
         
 }
