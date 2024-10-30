6a6450ef4a652f759858642dc98c27dfb4b0f49a
Diki Faturrohman <118197085+DikiFaturrohman@users.noreply.github.com>
2024-10-30 14:22:55 +0700

    Flash

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,7 +1,13 @@
 <div class="container mt-3" >
 
+<div class="row">
+    <div class="col-lg-6">
+        <?php Flasher::flash(); ?>
+    </div>
+</div>
+
  <div class="row">
-    <div class="col-6">
+    <div class="col-lg-6">
 
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
                 Tambah Data Mahasiswa
@@ -15,7 +21,7 @@
             <?php foreach( $data['mhs'] as $mhs ): ?>
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     <?= $mhs['nama']; ?>
-                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?> " class="badge text-bg-success">Detail</a>
+                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?> " class="badge text-bg-warning">Detail</a>
                 </li>
             <?php endforeach; ?>
             </ul>
